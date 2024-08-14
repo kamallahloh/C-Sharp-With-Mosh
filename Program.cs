1d3dab5179b2e4afb4fbd01d3c37a3817df37c47
KamalLahloh <kamal.lahloh@gmail.com>
2024-08-14 22:51:14 +0300

    OverFlowing

--- Program.cs
+++ Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //int number;
+            int Number = 1;
+            const float Pi = 3.14f;
+            // f for float not double the default datatype for real numbers.
+            decimal numberOne = 1.2m;
+
+
+            //////////////////////////////////////////////////
+            // OverFlow
+            checked
+            {
+                byte number = 255;
+                number++;
+            }
+
+            // SCOPE
+
+
+        }
         }
     }
 }
