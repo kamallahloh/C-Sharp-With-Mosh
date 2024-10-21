744c89f3c9fce9940d04fb80942cd17294a037a0
Kamal Lahloh <kamal.lahloh@gmail.com>
2024-10-21 12:56:44 +0300

    Hello World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // UDEMY C# Basics for Beginners: Learn C# Fundamentals by Coding
+            // https://www.udemy.com/course/csharp-tutorial-for-beginners/
+            //Created by Mosh Hamedani
+
+            /*
+             
+             .NET
+            framework
+            windows
+            f#
+            VB
+            CLR
+            Common language runtime
+            c++
+            compiler native code
+            not run on linux
+            lang bytecode IL code to bnative code by CLR 
+            CLR different machines
+
+
+            CLASSES 
+            class container
+            Data state attribute
+            Methods function 
+
+            related Classes => namespaces
+
+            related namesopaces => Assembly DLL dynamic linked Library
+
+            */
+            Console.WriteLine("Hello World");
+        }
+    }
+}
