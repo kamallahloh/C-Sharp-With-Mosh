d5e178fdf16d821e048b7b61183579c636d7d59f
Kamal Lahloh <kamal.lahloh@gmail.com>
2024-10-21 16:07:40 +0300

    Enums

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -57,41 +57,71 @@ namespace HelloWorld
 
             //// declare array     set size   
             ////int[] numbers = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
-            var numbers = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            //var numbers = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
             //Console.WriteLine(numbers[1]); // 2
             //Console.WriteLine(numbers[15]); // 0 default value /bool false /string "" 
             ////
 
-            // Strings   /// strings are Immutable XXX can't change it.
-            // String concatination
-            string fName = "Kamal";
-            string lName = "Lahloh";
-            // with Class String you need to import the String class first.
-            String lName2 = "Lahloh";
-            Int32 i = 5; int j = 6; // same thing with import Int32
+            //// Strings   /// strings are Immutable XXX can't change it.
+            //// String concatination
+            //string fName = "Kamal";
+            //string lName = "Lahloh";
+            //// with Class String you need to import the String class first.
+            //String lName2 = "Lahloh";
+            //Int32 i = 5; int j = 6; // same thing with import Int32
 
-            string name1 = fName + " " + lName;
-            string name2 = string.Format("{0} {1}", fName, lName);
-            string name3 = $"{fName} {lName}";
+            //string name1 = fName + " " + lName;
+            //string name2 = string.Format("{0} {1}", fName, lName);
+            //string name3 = $"{fName} {lName}";
 
-            Console.WriteLine("name3: " + name3);
+            //Console.WriteLine("name3: " + name3);
 
-            string list = string.Join(",", numbers);
-            Console.WriteLine(list);
+            //string list = string.Join(",", numbers);
+            //Console.WriteLine(list);
 
-            // Escape characters \n \t \\ \' \"
+            //// Escape characters \n \t \\ \' \"
 
-            // PATHS with Verbatim string ****
-            //string path = "c:\\desktop\\folder";
-            string path = @"c:\desktop\folder";
+            //// PATHS with Verbatim string ****
+            ////string path = "c:\\desktop\\folder";
+            //string path = @"c:\desktop\folder";
 
-
-
-
+            //Console.WriteLine(path);
 
 
+            // Enums a set of name/value pairs of related (constants)  
+            // ** declaer outside other method since it is a new type need to be defined at nameSpace level or Class level
+            Console.WriteLine(MyEnum.const1.ToString()); // const1
+            Console.WriteLine(MyEnum.const1); // const1  // Console.WriteLine apply ToString() to all its arguments
+            Console.WriteLine((int)MyEnum.const1); // 1
 
 
+            // casting Enums
+
+            var id = 2;
+            Console.WriteLine((MyEnum)id); // const2
+
+            var string1 = "const4";
+            //Enum.Parse(typeof(MyEnum), string1); // create an object need to be casted to our MyEnum
+            //var newEnum = (MyEnum)Enum.Parse(typeof(MyEnum), string1); // create an object need to be casted to our MyEnum
+            
+            //Console.WriteLine(newEnum);
+
+            // solve casting error
+            if (Enum.TryParse<MyEnum>(string1, out var newEnum2) && Enum.IsDefined(typeof(MyEnum), newEnum2))
+            {
+                Console.WriteLine(newEnum2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid enum value");
+            }
+
+        }
+        public enum MyEnum // basiclly it's a new Type 
+        {
+            const1 = 1, // = 0, default values but not best practice since the order maybe changed later
+            const2 = 2, // = 1,
+            const3 = 3, // = 2,
         }
     }
 }
